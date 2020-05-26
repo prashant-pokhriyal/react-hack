0089b44211410c176384e154c4875160c19b7d9c
Prashant Pokhriyal <pokhariyalp@gmail.com>
2020-05-26 22:01:12 +0530

    remove parent div from TheDotMatrix component

--- src/components/TheDotMatrix/index.js
+++ src/components/TheDotMatrix/index.js
@@ -38,24 +38,22 @@ function TheDotMatrix() {
 
 
   return (
-    <div>
-      <Grid container spacing={1} direction="row" justify="center" alignItems="center">
-        <Grid item lg={3} sm={5} md={4} xs={12}>
-          <Paper className={classes.paper} elevation="0">
-            <Controls onChange={handleChange} data={state}></Controls>
-          </Paper>
-        </Grid>
-        <Grid item lg={9} sm={7} md={8} xs={12} style={{
-          maxWidth: '100%',
-          maxHeight: '100%',
-          overflow: 'scroll'
-        }}>
-          <Paper className={classes.paper} elevation="0">
-            <Board {...state}></Board>
-          </Paper>
-        </Grid>
+    <Grid container spacing={1} direction="row" justify="center" alignItems="center">
+      <Grid item lg={3} sm={5} md={4} xs={12}>
+        <Paper className={classes.paper} elevation="0">
+          <Controls onChange={handleChange} data={state}></Controls>
+        </Paper>
       </Grid>
-    </div>
+      <Grid item lg={9} sm={7} md={8} xs={12} style={{
+        maxWidth: '100%',
+        maxHeight: '100%',
+        overflow: 'scroll'
+      }}>
+        <Paper className={classes.paper} elevation="0">
+          <Board {...state}></Board>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
 
